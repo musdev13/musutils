8570f1115d234bad2e98491f6a65bc2a85352595
MusDev13 <sonics0977703996@gmail.com>
2026-07-15 16:56:07 +0300

    get_json_str fix

--- src/http/get_json_str.rs
+++ src/http/get_json_str.rs
@@ -1,10 +1,13 @@
-pub async fn get_json_str(url: String) -> String{
-    serde_json::to_string_pretty(
-    reqwest::get(url)
+use serde_json::Value;
+
+pub async fn get_json_str(url: String) -> String {
+    let response: Value = reqwest::get(url)
         .await
         .expect("Failed to send request")
         .json()
         .await
         .expect("Failed to parse JSON response");
-    )
+
+    serde_json::to_string_pretty(&response)
+        .expect("Failed to serialize JSON to string")
 }
